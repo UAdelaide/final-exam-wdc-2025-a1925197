8211948e0d97a852dc466ca688b08c40668b37ea
Lachlan Mulyk <a1925197@adelaide.edu.au>
2025-06-20 08:02:35 +0000

    [automatic commit] 2025-06-20 08:02:35 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -7,7 +7,7 @@ const { authenticate } = require('./userRoutes');
 router.get('/', async function(req, res) {
    try {
     const [dogs] = await db.execute(`
-SELECT Dogs.name, Dogs.size, Users.username FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id;
+SELECT Dogs.dog_id, Dogs.name, Dogs.size, Users.user_id AS owner_id, FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id;
         `);
     res.json(dogs);
   } catch (err) {
@@ -20,7 +20,7 @@ router.get('/mydogs', authenticate, async function(req, res) {
 
    try {
     const [dogs] = await db.execute(`
-SELECT Dogs.dog_id, Dogs.name, Dogs.size, Users.user_id AS owner_id, FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE Users.user_id = ?;
+SELECT Dogs.name, Dogs.size, Users.username FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE Users.user_id = ?;
         `, [req.session.user.id]);
     res.json(dogs);
   } catch (err) {
