24cd0c7948599caf45d6d27ea55142224e962fdd
Lachlan Mulyk <a1925197@adelaide.edu.au>
2025-06-20 05:14:22 +0000

    [automatic commit] 2025-06-20 05:14:22 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -68,7 +68,7 @@ router.post('/login', async (req, res) => {
       role: rows[1]
     };
 
-    res.json({ message: 'Login successful', user: req.session.user, role: rows[1] });
+    res.json({ message: 'Login successful', user: req.session.user });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
   }
