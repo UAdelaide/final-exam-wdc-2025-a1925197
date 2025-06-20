87e804b6a5396998057aadeba992afc7ea69eef1
Lachlan Mulyk <a1925197@adelaide.edu.au>
2025-06-20 07:32:10 +0000

    [automatic commit] 2025-06-20 07:32:10 UTC

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -15,7 +15,7 @@ router.get('/', authenticate, async (req, res) => {
         JOIN Dogs d ON wr.dog_id = d.dog_id
         JOIN Users u ON d.owner_id = u.user_id
         WHERE wr.status = 'open' AND u.user_id = ?;
-      `);
+      `, [req.session.user.id]);
       res.json(rows);
     } catch (error) {
       console.error('SQL Error:', error);
