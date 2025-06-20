d580f5ea9cc0e19d83b4b2ff31483af429828fbf
Lachlan Mulyk <a1925197@adelaide.edu.au>
2025-06-20 00:39:22 +0000

    [automatic commit] 2025-06-20 00:39:22 UTC

--- part1/routes/api.js
+++ part1/routes/api.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/dogs', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/dogs', async function(req, res, next) {
+   try {
+    const [books] = await db.execute('SELECT * FROM books');
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch books' });
+  }
 });
 
 router.get('/walkrequests/open', function(req, res, next) {
