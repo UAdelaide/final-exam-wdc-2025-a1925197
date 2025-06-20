51b819c30bee99b57c5744fb7d102a7c337f48cf
Lachlan Mulyk <a1925197@adelaide.edu.au>
2025-06-20 07:29:07 +0000

    [automatic commit] 2025-06-20 07:29:07 UTC

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -5,6 +5,9 @@ const { authenticate } = require('./userRoutes');
 
 // GET all walk requests (for walkers to view)
 router.get('/', authenticate, async (req, res) => {
+
+  if()
+
   try {
     const [rows] = await db.query(`
       SELECT wr.*, d.name AS dog_name, d.size, u.username AS owner_name
