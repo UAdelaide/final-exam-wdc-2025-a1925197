98996617e1870967917f137554bc766353a02992
Lachlan Mulyk <a1925197@adelaide.edu.au>
2025-06-20 01:47:07 +0000

    [automatic commit] 2025-06-20 01:47:07 UTC

--- part1/app.js
+++ part1/app.js
@@ -204,7 +204,7 @@ WHERE Users.role = 'walker'
 
 LEFT JOIN WalkApplications ON Users.user_id = WalkApplications.walker_id
 LEFT JOIN WalkRequests ON WalkApplications.request_id = WalkRequests.request_id AND WalkRequests.status = 'completed'
-LEFT JOIN WalkRatings ON 
+LEFT JOIN WalkRatings ON WalkRatings.walker_id = WalkApplications.
         `);
     res.json(requests);
   } catch (err) {
