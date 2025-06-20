83cb98b9ad252dc71558b290a792ef3c6e4da4d8
Lachlan Mulyk <a1925197@adelaide.edu.au>
2025-06-20 05:03:13 +0000

    [automatic commit] 2025-06-20 05:03:13 UTC

--- part2/app.js
+++ part2/app.js
@@ -7,10 +7,12 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(session)
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+const session = require('express-session');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
