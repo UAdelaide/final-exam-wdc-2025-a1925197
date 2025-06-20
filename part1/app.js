c7f71627c2557394f2ceeb97185b9fe259ceb5cc
Lachlan Mulyk <a1925197@adelaide.edu.au>
2025-06-20 01:03:05 +0000

    [automatic commit] 2025-06-20 01:03:05 UTC

--- part1/app.js
+++ part1/app.js
@@ -159,7 +159,7 @@ SELECT Dogs.name, Dogs.size, Users.username FROM Dogs INNER JOIN Users ON Dogs.o
 app.get('/api/walkrequests/open', async function(req, res, next) {
      try {
     const [requests] = await db.execute(`
-SELECT WalkApplications.request_id, Dogs.name, WalkApplications.requested_time, WalkApplications.duration_minutes, WalkApplications.location, Users.username
+SELECT WalkApplications.request_id, Dogs.name AS dog_name, WalkApplications.requested_time, WalkApplications.duration_minutes, WalkApplications.location, Users.username
 FROM WalkApplications
 INNER JOIN Dogs ON WalkApplications.dog_id = Dogs.dog_id
 INNER JOIN Users ON Dogs.owner_id = Users.user_id
