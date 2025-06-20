7d9122a54d3dc54f707b2d1997422833c1d3ba08
Lachlan Mulyk <a1925197@adelaide.edu.au>
2025-06-20 01:35:53 +0000

    [automatic commit] 2025-06-20 01:35:53 UTC

--- part1/app.js
+++ part1/app.js
@@ -195,7 +195,8 @@ app.get('/api/walkers/summary', async function(req, res, next) {
     const [requests] = await db.execute(`
 SELECT Users.username AS walker_username,
 COUNT(WalkRatings.rating_id) AS total_ratings,
-AVG(WalkRatings.rating) AS 
+AVG(WalkRatings.rating) AS average_rating,
+SUM()
         `);
     res.json(requests);
   } catch (err) {
