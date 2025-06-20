e3ff18126e019ed7ba4ad3667fdc8b380e16b18b
Lachlan Mulyk <a1925197@adelaide.edu.au>
2025-06-20 00:51:16 +0000

    [automatic commit] 2025-06-20 00:51:16 UTC

--- part1/app.js
+++ part1/app.js
@@ -39,7 +39,6 @@ let db;
 
     // From dogwalks.sql, creating the data
     await db.execute(`
-USE DogWalkService;
 CREATE TABLE Users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
