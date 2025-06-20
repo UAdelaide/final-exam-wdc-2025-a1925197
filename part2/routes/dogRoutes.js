d082d1a9b5ba81e70fb769e8dd0647d9f9e17e10
Lachlan Mulyk <a1925197@adelaide.edu.au>
2025-06-20 07:39:14 +0000

    [automatic commit] 2025-06-20 07:39:14 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -19,8 +19,8 @@ SELECT Dogs.name, Dogs.size, Users.username FROM Dogs INNER JOIN Users ON Dogs.o
 router.get('/api/mydogs', authenticate, async function(req, res, next) {
    try {
     const [dogs] = await db.execute(`
-SELECT Dogs.name, Dogs.size, Users.username FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id;
-        `);
+SELECT Dogs.name, Dogs.size, Users.username FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id WHERE Users.user_id = ?;
+        `, );
     res.json(dogs);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch dogs' });
