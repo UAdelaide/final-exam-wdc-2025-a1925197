6db517e15693fb78d2b39f69b2de96c85c23373e
Lachlan Mulyk <a1925197@adelaide.edu.au>
2025-06-20 07:38:34 +0000

    [automatic commit] 2025-06-20 07:38:34 UTC

--- part2/routes/dogRoutes.js
+++ part2/routes/dogRoutes.js
@@ -4,7 +4,28 @@ const db = require('../models/db');
 const { authenticate } = require('./userRoutes');
 
 
+router.get('/api/dogs', async function(req, res, next) {
+   try {
+    const [dogs] = await db.execute(`
+SELECT Dogs.name, Dogs.size, Users.username FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id;
+        `);
+    res.json(dogs);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch dogs' });
+  }
+});
 
+
+router.get('/api/mydogs', async function(req, res, next) {
+   try {
+    const [dogs] = await db.execute(`
+SELECT Dogs.name, Dogs.size, Users.username FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id;
+        `);
+    res.json(dogs);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch dogs' });
+  }
+});
 
 
 module.exports = router;
